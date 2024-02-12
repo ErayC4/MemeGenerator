08fdb2e9c3094ca14a95e42835be8d58a38769ab
ErayC4 <eray04.coskun@gmail.com>
2024-02-12 16:24:52 +0100

    you can add a text to the images now

--- src/Memegenerator.jsx
+++ src/Memegenerator.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import memesData from "./memesData";
 
 function Memegenerator() {
+  const [memeImage, setMemeImage] = React.useState("");
 
-    const [memeImage, setMemeImage] = React.useState("")
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
 
-    function getMemeImage(){
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-    }
+  const [memeText, setMemeText] = React.useState({
+    topText: "",
+    bottomText: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setMemeText((prevMemeText) => ({
+      ...prevMemeText,
+      [name]: value,
+    }));
+  }
 
   return (
     <div>
@@ -30,22 +42,36 @@ function Memegenerator() {
             type="text"
             placeholder="upper text..."
             className="w-1/2 border border-gray-400 rounded-lg py-2 px-2"
+            name="topText"
+            value={memeText.topText}
+            onChange={handleChange}
           />
           <input
             type="text"
             placeholder="bottom text..."
             className="w-1/2 border border-gray-400 rounded-lg py-2 px-2"
+            name="bottomText"
+            value={memeText.bottomText}
+            onChange={handleChange}
           />
         </div>
 
-        <button onClick={getMemeImage} className="bg-yellow-400 w-full py-2 rounded-lg">
+        <button
+          onClick={getMemeImage}
+          className="bg-yellow-400 w-full py-2 rounded-lg"
+        >
           Get a new meme image
         </button>
 
-        <img
-          src={memeImage}
-          className="w-full py-4"
-        />
+        <div className="relative">
+          <img src={memeImage} className="w-full py-4" />
+          <p className="absolute top-0 w-full text-center mt-16 text-4xl ">
+            {memeText.topText}
+          </p>
+          <p className="absolute bottom-0 w-full text-center mb-16 text-4xl">
+            {memeText.bottomText}t
+          </p>
+        </div>
       </div>
     </div>
   );
